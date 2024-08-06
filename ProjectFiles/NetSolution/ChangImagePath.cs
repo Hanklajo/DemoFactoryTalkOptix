b038cb1d67fad85675c129957aa3e955a82343ff
Hanklajo <hankla666@gmail.com>
2024-08-07 03:12:08 +0700

    update

---
+++ ProjectFiles/NetSolution/ChangImagePath.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.OPCUAServer;
+using FTOptix.WebUI;
+#endregion
+
+public class ChangImagePath : BaseNetLogic
+{
+    public override void Start()
+    {
+        string PathDir = "";
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
